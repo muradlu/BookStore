f04679c767530d46c44725a1b60efb9701cddfee
MURAD MURADLI <160316049@ogr.cbu.edu.tr>
2021-03-16 16:07:44 +0400

    Added a home controller.

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controller
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Ramin is getting fucked Jacopo";
+        }
+    }
+}
